4e156f62b680d53e2601504ba82f7b5136ce0b7a
Ping <silentchinasilent@gmail.com>
2016-05-14 17:31:07 -0700

    Add files via upload
    "my solution"

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,9 @@
 
 ## Write a short comment describing this function
 
+#create a special "vector", which is really a list containing a function to set the matrix, 
+#get the matrix, set the inverse of the matrix, get the inverse of the matrix
+
 makeCacheMatrix <- function(x = maxtrix()) {
   inv <- NULL
   set <- function(y) {
@@ -19,7 +22,9 @@ makeCacheMatrix <- function(x = maxtrix()) {
 
 
 ## Write a short comment describing this function
-
+#Compute the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), 
+#then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinv()
